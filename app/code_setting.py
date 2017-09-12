e5af524745a0da1d4f53d44576058c4281b5d865
Han <han@Hanui-MacBook-Pro.local>
2017-09-12 18:55:51 +0900

    remove ps1, ps2 color

--- app/code_setting.py
+++ app/code_setting.py
@@ -8,8 +8,10 @@ class PS1LineCounter:
 
     def __str__(self):
         self.count += 1
-        return '\001\033[94m\002[APC {:3d}] >> \001\033[0m\002'.format(self.count)
+        return '[APC {:3d}] >> '.format(self.count)
+        # return '\001\033[94m\002[APC {:3d}] >> \001\033[0m\002'.format(self.count)
 
 class PS2LineCounter:
     def __str__(self):
-        return '\001\033[94m\002          .. \001\033[0m\002'
+        return '          .. '
+        # return '\001\033[94m\002          .. \001\033[0m\002'
