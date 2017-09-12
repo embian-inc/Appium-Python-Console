aad281e88cdb8871c4d97dbd9bd035a3f686d0e1
Han <han@Hanui-MacBook-Pro.local>
2017-09-12 19:04:27 +0900

    print_action_table > split action table & action group table

--- app/command_runner.py
+++ app/command_runner.py
@@ -1,18 +1,17 @@
 #-*- coding: utf-8 -*-
 import unittest, time, os, re, codecs, sys, imp, errno
+import native_manager, webview_manager, command
+import code
+
 from time import sleep
 from appium import webdriver
 from terminaltables import AsciiTable
-import native_manager, webview_manager, command
+from bs4 import BeautifulSoup
+
 # from owyl_bt import ApptestBT, ApptestBTPageDetect
 # from owyl_bt.owyl import blackboard
 
-from bs4 import BeautifulSoup
-
-import code
-
 # monkey patching prettify
-
 _stdin_encoding = sys.stdin.encoding or 'utf-8'
 orig_prettify = BeautifulSoup.prettify
 r = re.compile(r'^(\s*)', re.MULTILINE)
@@ -22,42 +21,63 @@ def prettify(self, encoding=None, formatter="minimal", indent_width=4):
 BeautifulSoup.prettify = prettify
 
 class CommandRunner(object):
-    behavior = None
+    ## for Behavior Tree
     # apptestBTPageDetect = None
     # bb = blackboard.Blackboard('apptest')
     # bb['last_cmd'] = []
+    behavior = None
+
     r_cnt = 0
-    ACTION_DESC_RE = re.compile(r'/alias\:(.*)/pos\:(.*)/widget\:(.*)$')
-    ACTION_GROUP_DESC_RE = re.compile(r'/alias\:(.*)/pos\:(.*)$')
 
     def __init__(self):
         self.line_num = 1
         self.screen_id = 'screen-0'
         self.stored_opt = {'detail': False}
 
-    #show actions
-    def print_action_table(self, pagedata, detail=None):
+    # Print action list table
+    def print_action_table(self, pagedata, detail=None, mode=None):
         actions = pagedata['actions'] if 'actions' in pagedata else []
-        action_groups = pagedata['action_groups'] if 'action_groups' in pagedata else []
+        header = ['No.','WIDGET', 'RESOURCE-ID', 'CONTENT-DESC', 'BOUNDS','ACTION', 'CONTEXT']
+        if mode == 'dev':
+            header = ['No.', 'DESC', 'LABEL', 'ACTION', 'BOUNDS', 'CONTEXT']
 
         print '\n'
         table = []
-        header = ['No.', 'DESC', 'LABEL', 'ACTION', 'INPUT-TYPE', 'RESOURCE-ID', 'BOUNDS', 'CONTEXT']
+        keys = header[1:-1]
         if self.stored_opt['detail'] or detail == 'd':
             header.append('XPATH')
-
         table.append(header)
+
         for cnt, el in enumerate(actions):
             input_type = '-' if el['input-type'] == 'null' else el['input-type']
+            # [
+            #     '%03d' % cnt,
+            #     el['desc'],
+            #     el['label'],
+            #     el['action'],
+            #     el['widget'],
+            #     el['resource-id'],
+            #     el['bounds'],
+            #     el['type']
+            # ]
+            row = ['%03d' % cnt]
+            for key in keys:
+                row.append(el[key.lower()])
+            row.append(el['type'])
+
             if self.stored_opt['detail'] or detail == 'd':
-                table.append(['%03d' % cnt, el['desc'], el['label'], el['action'], input_type, el['resource-id'], el['bounds'], el['type'], el['xpath']])
+                row.append(el['xpath'])
+                table.append(row)
             else:
-                table.append(['%03d' % cnt, el['desc'], el['label'], el['action'], input_type, el['resource-id'], el['bounds'], el['type']])
+                table.append(row)
 
         at = AsciiTable(table, title="Action List")
-        # at.inner_row_border = True
         print at.table
+        print '\n'
 
+    # Print action group list table
+    def print_action_group_table(self, pagedata):
+        action_groups = pagedata['action_groups'] if 'action_groups' in pagedata else []
 
         print '\n'
         table = []
@@ -95,6 +115,7 @@ class CommandRunner(object):
         data['driver'].save_screenshot(png_filename)
         data['driver'].switch_to.context(orig_context)
         print '# Screen Shot Save OK'
+        print '%s' % png_filename
 
     def xml_doc_save(self, data, idx=None):
         if data['now_context_num'] != 0:
@@ -109,6 +130,7 @@ class CommandRunner(object):
             xml = data['driver'].page_source
             xml_file.write(xml)
         print '# XML File Save OK'
+        print '%s' % xml_filename
 
     def html_doc_save(self, data, idx=None):
         for c in data['driver'].contexts:
@@ -122,14 +144,15 @@ class CommandRunner(object):
                 with codecs.open(html_filename, 'w', 'utf-8') as html_file:
                     html_file.write(data['driver'].page_source)
         print '# HTML File Save OK'
+        print '%s' % html_filename
 
 
     # if result is true, finish test
     def do_command(self, data):
         extra_sleep=0
-        print ''
-        print '# Manual Test Command 정보 - help 또는 h 를 입력하세요.'
+        print "\n# About Manual Test Command - Enter 'help' or 'h' "
 
+        print '[Current Context : %s]' % data['driver'].context
         promp = '\n[App-Manual-Test] Input Action Number #%d >> ' % self.line_num
 
         self.line_num = self.line_num + 1
@@ -145,8 +168,27 @@ class CommandRunner(object):
                 idx= -1
 
             elif command.is_page_source(choice):
-                soup = BeautifulSoup(data['driver'].page_source, "xml")
-                print soup.prettify()
+                i = 0
+                print "\nChoose Contexts List to bottom:"
+                for c in data['contexts']:
+                    print "%d. %s" % (i, c)
+                    i = i + 1
+
+                c_num = raw_input('\nContexts Number >> ')
+                try:
+                    num = int(c_num)
+                except:
+                    return False
+                bs4_type = "xml" if data['contexts'][int(c_num)] == 'NATIVE_APP' else "lxml"
+                now_context_num = data['now_context_num']
+                orig_context = data['contexts'][now_context_num]
+                data['driver'].switch_to.context(data['contexts'][int(c_num)])
+                soup = BeautifulSoup(data['driver'].page_source, bs4_type, from_encoding='utf-8')
+                print '\n'
+                print soup.prettify
+                print '\n'
+
+                data['driver'].switch_to.context(orig_context)
                 return False
 
             elif command.is_detail(choice):
@@ -195,7 +237,7 @@ class CommandRunner(object):
                 idx= -1
 
             elif command.is_exit(choice):
-                print 'exit'
+                print 'Terminate Manual Test Mode.\n'
                 return True
 
             # elif command.is_load(choice):
