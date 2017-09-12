b7ee3de5d7f2a5957d59bb816b72862538bcd359
Han <han@Hanui-MacBook-Pro.local>
2017-09-12 18:58:38 +0900

    .

--- app/webview_manager.py
+++ app/webview_manager.py
@@ -9,9 +9,7 @@ input_type_text_list = ["text", "search", "password", "textarea", "email", "numb
 input_type_etc_list = ["date", "color", "datetime", "month", "range", "time", "week"]
 
 def set_js_script():
-    file_dir = os.path.dirname(os.path.realpath('__file__'))
-    print file_dir
-    file_path = os.path.join(file_dir, 'app/execute_script.js')
+    file_path = os.path.join('app', 'apptestai_modules', 'webview', 'execute_script.js')
     with open(file_path, 'r') as ac_script:
         js_script = ac_script.read()
 
@@ -41,18 +39,14 @@ def get_elements(driver, curr_context):
     elements = []
     for w in ws:
         driver.switch_to_window(w)
-        # t1 = time()
         res = driver.execute_script(script, curr_context)
-        # t2 = time()
-        # print '\nEcecute Script Running time : ', t2-t1
-        # print '\nElapsed time (Js for loop): ', res['elapsed_time']
+
         act = {
             'key': w,
             'window_width': res['window_width'],
             'window_height': res['window_height'],
             'actions': res['action_list']
         }
-
         el = {
             'key': w,
             'window_width': res['window_width'],
@@ -87,28 +81,20 @@ def exec_action(data, idx):
             input_val = raw_input("# Enter the Input Value >> ").decode(_stdin_encoding)
         print '=' * 80
         data['driver'].execute_script('arguments[0].value = arguments[1];', action['target'], input_val)
-        # target_element.clear()
-        # target_element.send_keys(input_val)
+
     elif action_type == 'spinner':
         print ( '# Selected Spinner Action >> %03d. %s - %s %s' % (idx, str(action_type), str(target_element.tag_name), target_element.text) )
         print '=' * 80
-
         target_element.click()
 
     elif action_type == 'input' and input_type in input_type_etc_list:
         print ( '# Selected Input_%s Action >> %03d. %s - %s %s' % (action_type, idx, str(action_type), str(target_element.tag_name), target_element.text) )
         print '=' * 80
-
         target_element.click()
 
     elif action_type == 'click' or action_type == 'checkbox':
         print ( '# Selected Execute_Script Click Action >> %03d. %s - %s %s' % (idx, str(action_type), str(target_element.tag_name), target_element.text) )
         print '=' * 80
         data['driver'].execute_script('if(!arguments[0].checked) { arguments[0].click(); }', action['target'])
-
-        # ac = ActionChains(data['driver'])
-        # ac.move_to_element(target_element).click().perform()
-
-        # target_element.click()
 
     return True
